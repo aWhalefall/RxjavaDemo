6745d80df1e6af5e876240a2a8fdfa48e5f878ed
yatou252303 <279642707@qq.com>
2017-05-23 14:41:48 +0800

    增加连接 ，背压，转换 rx操作方式

---
+++ app/src/main/java/com/nuoyuan/rxdemo/category2_transforming_observables/Category2_TransformingObservablesActivity.java
@@ -0,0 +1,69 @@
+package com.nuoyuan.rxdemo.category2_transforming_observables;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.nuoyuan.rxdemo.R;
+import com.nuoyuan.rxdemo.base.BaseActivity;
+
+
+
+public class Category2_TransformingObservablesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_category2_transforming_observables);
+    }
+
+    /**
+     * 2.1 map
+     * @param view
+     */
+    public void jumpToCategory2Operator1MapActivity(View view) {
+        startActivity(new Intent(this, Category2Operator1MapActivity.class));
+    }
+
+    /**
+     * 2.2 flatMap
+     * @param view
+     */
+    public void jumpToCategory2Operator2FlatMapActivity(View view) {
+        startActivity(new Intent(this, Category2Operator2FlatMapActivity.class));
+    }
+
+    /**
+     * 2.3 scan
+     * @param view
+     */
+    public void jumpToCategory2Operator3ScanActivity(View view) {
+        startActivity(new Intent(this, Category2Operator3ScanActivity.class));
+    }
+
+    /**
+     * 2.4 groupBy
+     * @param view
+     */
+    public void jumpToCategory2Operator4GroupByActivity(View view) {
+        startActivity(new Intent(this, Category2Operator4GroupByActivity.class));
+    }
+
+    /**
+     * 2.5 buffer
+     * @param view
+     */
+    public void jumpToCategory2Operator1BufferActivity(View view) {
+        startActivity(new Intent(this, Category2Operator5BufferActivity.class));
+    }
+
+    /**
+     * 2.6 window
+     * @param view
+     */
+    public void jumpToCategory2Operator6WindowActivity(View view) {
+        startActivity(new Intent(this, Category2Operator6WindowActivity.class));
+    }
+}
